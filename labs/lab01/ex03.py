4cee163b3c62187a54aca9865b8410b9bddc9da5
Filippo Corti <filippo.corti1@studenti.unimi.it>
2026-09-24 16:40:46 +0200

    ex03 p1

---
+++ labs/lab01/ex03.py
@@ -0,0 +1,63 @@
+CELSIUS = "Celsius"
+FAHRENEIT = "Fahreneit"
+KELVIN = "Kelvin"
+RANKINE = "Rankine"
+DELISLE = "Delisle"
+NEWTON = "Newton"
+REAUMUR = "Reaumur"
+ROMER = "Romer"
+
+
+scales = [CELSIUS, FAHRENEIT, KELVIN, RANKINE, DELISLE, NEWTON, REAUMUR, ROMER]
+# uom = ['°C', '°F', 'K', '°R', '°De', '°N', '°Re', '°Ro']
+
+
+kelvin_to_any = {
+    CELSIUS: lambda x: (x - 273.15),
+    FAHRENEIT: lambda x: (x * 9 / 5 - 459.67),
+    KELVIN: lambda x: x,
+    RANKINE: lambda x: (x * 9 / 5),
+    DELISLE: lambda x: (373.15 - x) * 3 / 2,
+    NEWTON: lambda x: (x - 273.15) * 33 / 100,
+    REAUMUR: lambda x: (x - 273.15) * 4 / 5,
+    ROMER: lambda x: ((x - 273.15) * 21 / 40 + 7.5),
+}
+
+any_to_kelvin = {
+    CELSIUS: lambda x: (x + 273.15),
+    FAHRENEIT: lambda x: (x + 459.67) * 5 / 9,
+    KELVIN: lambda x: x,
+    RANKINE: lambda x: (x * 5 / 9),
+    DELISLE: lambda x: (373.15 - x * 2 / 3),
+    NEWTON: lambda x: (x * 100 / 33 + 273.15),
+    REAUMUR: lambda x: (x * 5 / 4 + 273.15),
+    ROMER: lambda x: ((x - 7.5) * 40 / 21 + 273.15),
+}
+
+# 1)
+
+convert = lambda n, fr, to: kelvin_to_any[to](any_to_kelvin[fr](n))
+
+
+def table(n, colsize=12):
+    tablerow = lambda l: "|" + "|".join(l) + "|\n"
+
+    header = tablerow(f"{s:^{colsize}}" for s in scales)
+    hline = tablerow(["-" * colsize] * len(scales))
+
+    return (
+        hline
+        + header
+        + hline
+        + "".join(
+            tablerow(
+                f"{convert(n, fromscale, toscale):+{colsize}.3f}" for toscale in scales
+            )
+            for fromscale in scales
+        )
+        + hline
+    )
+
+
+NUMBER = 56
+print(table(NUMBER, colsize=12))
