eebdef5edbed5f60c770202b16c2bfb68f17f497
Filippo Corti <filippo.corti1@studenti.unimi.it>
2026-09-24 16:56:24 +0200

    ex3 part 2

--- labs/lab01/ex03.py
+++ labs/lab01/ex03.py
@@ -9,7 +9,16 @@ ROMER = "Romer"
 
 
 scales = [CELSIUS, FAHRENEIT, KELVIN, RANKINE, DELISLE, NEWTON, REAUMUR, ROMER]
-# uom = ['°C', '°F', 'K', '°R', '°De', '°N', '°Re', '°Ro']
+uom = {
+    CELSIUS: "°C",
+    FAHRENEIT: "°F",
+    KELVIN: "K",
+    RANKINE: "°R",
+    DELISLE: "°De",
+    NEWTON: "°N",
+    REAUMUR: "°Re",
+    ROMER: "°Ro",
+}
 
 
 kelvin_to_any = {
@@ -34,9 +43,9 @@ any_to_kelvin = {
     ROMER: lambda x: ((x - 7.5) * 40 / 21 + 273.15),
 }
 
-# 1)
-
 convert = lambda n, fr, to: kelvin_to_any[to](any_to_kelvin[fr](n))
+
+# 1)
 
 
 def table(n, colsize=12):
@@ -59,5 +68,27 @@ def table(n, colsize=12):
     )
 
 
+# 2)
+
+
+def toAll(n, scale):
+    if scale not in scales:
+        raise Exception("Unknown scale")
+
+    conversions = sorted(
+        (
+            (convert(n, scale, toscale), toscale)
+            for toscale in scales
+            if toscale != scale
+        ),
+        key=lambda x: x[0],
+    )
+
+    return f"{n} {uom[scale]} = " + "; ".join(
+        f"{val:+.3f} {uom[s]}" for val, s in conversions
+    )
+
+
 NUMBER = 56
 print(table(NUMBER, colsize=12))
+print(toAll(NUMBER, CELSIUS))
